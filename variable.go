f548ad71332903b72bbfac65df97ebf5a28a474f
Ramdoni <doni.engine@gmail.com>
2022-10-14 05:42:40 +0700

    variable

---
+++ variable.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var (
+		nama   = "Budi"
+		umur   = 25
+		alamat = "Bojong Gede"
+	)
+
+	fmt.Println("Nama", nama)
+	fmt.Println("Umur", umur)
+	fmt.Println("Alamat", alamat)
+}
