bc5d1220c8c3a76cfc49ade7dcaf5bf70d0c4bec
Ramdoni <doni.engine@gmail.com>
2022-10-17 00:11:40 +0700

    update exported

---
+++ exported_names.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println("E : ", math.E)
+	fmt.Println("Ln10 : ", math.Ln10)
+	fmt.Println("Ln2 : ", math.Ln2)
+	fmt.Println("Log10E : ", math.Log10E)
+	fmt.Println("MaxFloat32 : ", math.MaxFloat32)
+	fmt.Println("Pi : ", math.Pi)
+}
